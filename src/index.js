0b5a01221e0c55ac1e5e1051a2b9050507b681be
Daniel Waltrip <dwaltrip77@gmail.com>
2017-05-08 12:19:53 -0700

    Implement closing panel by clicking outside of it

--- src/index.js
+++ src/index.js
@@ -4,10 +4,37 @@ export default {
   oninit: function({ attrs:{selectedOption, options} }) {
     this.selectedOption = selectedOption || options[0];
     this.isOpen = false;
+
+    this.closePanelWithoutSideEffects = event => {
+      // Only if the mousedown is outside the container
+      console.log('closePanelWithoutSideEffects')
+      if (this.isOpen && !this.containerElement.contains(event.target)) {
+        console.log('closePanelWithoutSideEffects -- inside if')
+        this.close();
+        // prevent the mousedown from affecting any other elements
+        blockEvent(event);
+        // This will fire immediately, as 'mousedown' is before 'click'
+        addOneTimeHandler(document, 'click', blockEvent, true);
+        // Set useCapture=true to catch the event before any other existing click handlers
+        // The mousedown/click events should only close the panel, and not affect other elements
+        m.redraw();
+      }
+    }
+  },
+  oncreate: function(vnode) {
+    this.containerElement = vnode.dom;
+  },
+  onRemove: function() {
+    document.removeEventListener('mousedown', this.closePanelWithoutSideEffects, true);
   },
 
-  open:   function() { this.isOpen = true; },
-  close:  function() { this.isOpen = false; },
+  open: function() {
+    this.isOpen = true;
+    document.addEventListener('mousedown', this.closePanelWithoutSideEffects, true);
+  },
+  close: function() {
+    this.isOpen = false;
+  },
 
   renderOption: function(option) {
     var isSelected = option.value === this.selectedOption.value;
@@ -33,3 +60,15 @@ export default {
   }
 };
 
+function addOneTimeHandler(target, eventName, handler, useCapture=false) {
+  function oneTimeHandler(event)  {
+    handler(event);
+    target.removeEventListener(eventName, oneTimeHandler, useCapture);
+  };
+  target.addEventListener(eventName, oneTimeHandler, useCapture);
+}
+
+function blockEvent(event) {
+  event.stopImmediatePropagation();
+  event.preventDefault();
+}
